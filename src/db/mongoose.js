f24e450900cf9ad807481a8af20d42f22f333123
Jackielyn Benitez <jackielyn.benitez@accenture.com>
2021-12-12 23:27:18 +0800

    update db

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose')
 //     useNewUrlParser: true,
 // })
 
-const MONGODB_URI = "mongodb+srv://jackielyn_benitez:Kcaj725123@mycluster.mhydu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-mongoose.connect(MONGODB_URI || "mongodb://127.0.0.1:27017/MyDiary", {
+// const MONGODB_URI = "mongodb+srv://jackielyn_benitez:Kcaj725123@mycluster.mhydu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect("mongodb+srv://jackielyn_benitez:Kcaj725123@mycluster.mhydu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
     useNewUrlParser: true,
 })
 
